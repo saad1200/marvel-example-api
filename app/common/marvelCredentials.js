0cd95cbd81a944504ae032996aa66bb14f8dc75d
saad1200 <saad1200@gmail.com>
2014-10-14 00:47:54 +0100

    initial site

---
+++ app/common/marvelCredentials.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('common').service('marvelCredentials', ['md5', marvelCredentials]);
+
+    function marvelCredentials(md5) {
+        this.getKey = function () {
+            var publickey = 'd3573d7eec576d9eb8a2a83632750907';
+            var privatekey = '55e20985f35b98ad5c690e3dc9dde26a8289ea03';
+            var timestamp = new Date().getTime();
+            var hash = md5.createHash(timestamp + privatekey + publickey);
+            return 'ts=' + timestamp + '&apikey=' + publickey + '&hash=' + hash;
+        }
+    }
+})();
---
+++ app/marvel/marvel.js
@@ -0,0 +1,42 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'marvel';
+    angular.module('app').controller(controllerId, ['common', 'datacontext', 'charactersService', marvel]);
+
+    function marvel(common, datacontext, charactersService) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        vm.characters = [];
+
+        activate();
+
+        function activate() {
+            var promises = [getCharacters()];
+            common.activateController(promises, controllerId)
+                .then(function () { log('Activated Dashboard View'); });
+        }
+
+        function getCharacters() {
+            charactersService.getAll().success(function (result) {
+                console.log(result.data.results);
+                vm.characters = result.data.results;
+                return result.data;
+            });
+        }
+    };
+
+})();
+
+(function () {
+    'use strict';
+
+    angular.module('app').service('charactersService', ['$http', 'marvelCredentials', charactersService]);
+
+    function charactersService($http, marvelCredentials) {
+        this.getAll = function () {
+            return $http.get('http://gateway.marvel.com:80/v1/public/characters?' + marvelCredentials.getKey());
+        }
+    }
+})();
